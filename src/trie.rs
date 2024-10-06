71d4e7e14865c00c14ff02a30aa00da5b4bcfc8f
Can Vardar <ismailcanvardar@gmail.com>
2024-10-06 23:20:32 +0300

    test(trie): update test suite

--- src/trie.rs
+++ src/trie.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+use crate::errors::TrieError;
+
 #[derive(Debug)]
 pub struct TrieNode {
     children: HashMap<char, Option<Box<TrieNode>>>,
@@ -32,16 +34,16 @@ impl Trie {
         }
     }
 
-    pub fn insert(word: String, node: &mut TrieNode, position: usize) {
+    pub fn insert(word: String, node: &mut TrieNode, position: usize) -> Result<(), TrieError> {
         if position == word.len() {
             node.word_ends = true;
-            return;
+            return Ok(());
         }
 
         let c = word.as_bytes()[position] as char;
 
         if !c.is_ascii_alphabetic() {
-            panic!("Invalid ASCII character!");
+            return Err(TrieError::InvalidCharacter);
         }
 
         let child = node
@@ -50,30 +52,34 @@ impl Trie {
             .or_insert_with(|| Some(Box::new(TrieNode::new())));
 
         if let Some(child) = child {
-            Self::insert(word, child.as_mut(), position + 1);
+            Self::insert(word, child.as_mut(), position + 1)?;
         }
+
+        Ok(())
     }
 
-    pub fn search(word: String, node: &TrieNode, position: usize) -> bool {
+    pub fn search(word: String, node: &TrieNode, position: usize) -> Result<bool, TrieError> {
         if position == word.len() {
-            return node.word_ends;
+            return Ok(node.word_ends);
         }
 
         let c = word.as_bytes()[position] as char;
 
         if !c.is_ascii_alphabetic() {
-            panic!("Invalid ASCII character!");
+            return Err(TrieError::InvalidCharacter);
         }
 
         match node.children.get(&c) {
             Some(child) => Self::search(word, child.as_deref().unwrap(), position + 1),
-            None => false,
+            None => Ok(false),
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::error::Error;
+
     use super::*;
 
     #[test]
@@ -93,14 +99,29 @@ mod tests {
     }
 
     #[test]
-    fn it_inserts_word_to_trie() {
+    fn it_inserts_word_to_trie() -> Result<(), Box<dyn Error>> {
         let mut trie = Trie::new();
         let word = "test".to_string();
 
-        Trie::insert(word.clone(), &mut trie.root, 0);
+        Trie::insert(word.clone(), &mut trie.root, 0)?;
 
-        let has_word = Trie::search(word.clone(), &trie.root, 0);
+        let has_word = Trie::search(word.clone(), &trie.root, 0)?;
 
         assert!(has_word);
+
+        Ok(())
+    }
+
+    #[test]
+    fn it_inserts_word_with_non_ascii_character() -> Result<(), Box<dyn Error>> {
+        let mut trie = Trie::new();
+        let word = "~~~".to_string();
+
+        assert_eq!(
+            Trie::insert(word.clone(), &mut trie.root, 0).unwrap_err(),
+            TrieError::InvalidCharacter
+        );
+
+        Ok(())
     }
 }
