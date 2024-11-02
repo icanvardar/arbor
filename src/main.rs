08576598ffd29098ae58fb1ffdfafb15ceee47de
Can Vardar <ismailcanvardar@gmail.com>
2024-11-02 22:03:56 +0300

    chore: implement Arbor to main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use arbor::common::app_builder::{AppBuilder, Arbor};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    Arbor::build().await?;
+
+    Ok(())
 }
